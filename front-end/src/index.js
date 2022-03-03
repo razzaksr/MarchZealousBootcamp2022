fd384a77eb093ac9c8a58258d83c55bf4b8bc976
razzaksr <razzaksr@gmail.com>
2022-03-03 11:36:40 +0530

    project created and executed

---
+++ front-end/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+// render("what to show","where to show")
+ReactDom.render(
+    <>
+        <h1>Welcome to Zealous Bootcamp</h1>
+        <p>Razak Mohamed S</p>
+        <h2>Zealous Tech Corp</h2>
+    </>
+    ,document.getElementById("root")
+)
